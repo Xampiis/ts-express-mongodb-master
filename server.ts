229b9c3f3e4b0a58edc0f77485f250a1b16145bd
Bruno Jacomini <brunosjacomini18@gmail.com>
2021-05-28 21:15:08 -0300

    Curso TS

---
+++ server.ts
@@ -0,0 +1,28 @@
+import 'dotenv/config'; 
+import cors from 'cors';
+import express from 'express';
+import mongoose from 'mongoose';
+import morgan from 'morgan';
+
+import router from './api/routes/index';
+
+const { PORT, DB_URL } = process.env;
+
+// Create the express instance
+const app = express();
+
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect(DB_URL || '', { useNewUrlParser: true });
+
+// Middlewares
+app.use(morgan('dev')); // log das requisições
+app.use(express.json()); //Used to parse JSON bodies
+app.use(express.urlencoded()); //Parse URL-encoded bodies
+app.use(cors());
+
+// Routes which should handle requests
+router(app);
+
+// Listen for requests in the given port
+app.listen(PORT);
